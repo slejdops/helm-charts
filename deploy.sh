cfe461b9e9843a4dccf6886143c7e8681d409ed2
slejdops <slejdops@gmail.com>
2018-06-12 15:08:52 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 openssl aes-256-cbc -K $encrypted_2ba986d7921c_key -iv $encrypted_2ba986d7921c_iv -in helm-chart-deploy-key.enc -out helm-chart-deploy-key -d
-openssl aes-256-cbc -K $encrypted_058f1ad78f7f_key -iv $encrypted_058f1ad78f7f_iv -in deploy-key.rsa.enc -out deploy-key.rsa -d
+#openssl aes-256-cbc -K $encrypted_058f1ad78f7f_key -iv $encrypted_058f1ad78f7f_iv -in deploy-key.rsa.enc -out deploy-key.rsa -d
 chmod 0400 deploy-key.rsa
 
 helm init --client-only
