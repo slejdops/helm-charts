28ad3e176dfebc1e702f6676bf933ba056caf46c
slejdops <slejdops@gmail.com>
2018-06-14 11:03:12 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+openssl aes-256-cbc -K $encrypted_499b71e625dc_key -iv $encrypted_499b71e625dc_iv -in github_deploy_key.enc -out github_deploy_key -d
+chmod 0400 github_deploy_key
 
 helm init --client-only
 helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
